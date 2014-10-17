e7cccc131a7ae336e10d7c0c477f28113a46daaa
Waldir Pimenta <waldir@email.com>
2014-10-17 21:40:44 +0100

    convert into generic site, upgrade normalize.css

---
+++ js/age.js
@@ -0,0 +1,10 @@
+// born 8:15am local time in Leningrad (UTC+3) -->
+function dynamicAge() {
+	var today=new Date();
+	var bday=new Date("February 16, 1986 05:15:00");
+	var age=((today-bday)/1000/60/60/24/365).toFixed(9);
+
+	document.getElementById('age').innerHTML=age + "year-old";
+	t=setTimeout('dynamicAge()',100);
+}
+dynamicAge();
