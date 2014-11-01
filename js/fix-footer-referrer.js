0e8d317357829aa0f299d84c1bd6a9790750bc81
Waldir Pimenta <waldyrious@gmail.com>
2014-11-01 20:18:56 +0000

    testing another approach...

--- js/fix-footer-referrer.js
+++ js/fix-footer-referrer.js
@@ -2,7 +2,11 @@
 // Should be called "referer", but I refuse to obey a spec term
 // that is based on a misspelling: https://en.wiktionary.org/wiki/referer#Usage_notes
 
-function fixReferrer(footer){
-	footer.contentWindow.postMessage( ['referrer', window.location.href], '*' );
-	console.log(footer.src);
-}
+var iframe = document.getElementById('footer');
+
+//Wait for the iframe to send us the OK signal
+iframe.addEventListener('message', function (e) {
+	if (e.data[0] == 'fixReferrer') {
+		iframe.contentWindow.postMessage( ['referrer', window.location.href], '*' );
+	}
+}, false);
