90bb0951e2043b170f076edf1f7af740e3d41214
Waldir Pimenta <waldyrious@gmail.com>
2014-11-01 20:01:26 +0000

    don't message the iframe until it's ready

--- js/fix-footer-referrer.js
+++ js/fix-footer-referrer.js
@@ -1,6 +1,9 @@
 // Code from: http://benvinegar.github.io/seamless-talk/#/24
 // Should be called "referer", but I refuse to obey a spec term
 // that is based on a misspelling: https://en.wiktionary.org/wiki/referer#Usage_notes
+
 var iframe = document.getElementById('footer');
 
-iframe.contentWindow.postMessage( ['referrer', window.location.href], '*' );
+iframe.onload = function() {
+	iframe.contentWindow.postMessage( ['referrer', window.location.href], '*' );
+}
