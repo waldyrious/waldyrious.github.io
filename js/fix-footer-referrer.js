68125dcc0ab44b2f40818176c28f1173e8fff9fe
Waldir Pimenta <waldyrious@gmail.com>
2014-11-01 20:08:07 +0000

    a more scalable approach to update the footer

--- js/fix-footer-referrer.js
+++ js/fix-footer-referrer.js
@@ -2,8 +2,6 @@
 // Should be called "referer", but I refuse to obey a spec term
 // that is based on a misspelling: https://en.wiktionary.org/wiki/referer#Usage_notes
 
-var iframe = document.getElementById('footer');
-
-iframe.onload = function() {
-	iframe.contentWindow.postMessage( ['referrer', window.location.href], '*' );
+function fixReferrer(footer){
+	footer.contentWindow.postMessage( ['referrer', window.location.href], '*' );
 }
