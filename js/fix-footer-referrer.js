9443ad47e70b1ca0d40efb61fdbaca3b37a75a49
Waldir Pimenta <waldyrious@gmail.com>
2014-11-01 19:52:07 +0000

    attempt to fix footer validation link

---
+++ js/fix-footer-referrer.js
@@ -0,0 +1,6 @@
+// Code from: http://benvinegar.github.io/seamless-talk/#/24
+// Should be called "referer", but I refuse to obey a spec term
+// that is based on a misspelling: https://en.wiktionary.org/wiki/referer#Usage_notes
+var iframe = document.getElementById('footer');
+
+iframe.contentWindow.postMessage( ['referrer', window.location.href], '*' );
