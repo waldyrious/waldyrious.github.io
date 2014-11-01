6bd06451ef875aa9769ab341024bd4d80fea5b92
Waldir Pimenta <waldyrious@gmail.com>
2014-11-01 20:29:03 +0000

    Revert last two commits
    Didn't work. Will leave it at that and investigate later

--- js/fix-footer-referrer.js
+++ js/fix-footer-referrer.js
@@ -2,11 +2,6 @@
 // Should be called "referer", but I refuse to obey a spec term
 // that is based on a misspelling: https://en.wiktionary.org/wiki/referer#Usage_notes
 
-var iframe = document.getElementById('footer');
-
-//Wait for the iframe to send us the OK signal
-iframe.addEventListener('message', function (e) {
-	if (e.data[0] == 'fixReferrer') {
-		iframe.contentWindow.postMessage( ['referrer', window.location.href], '*' );
-	}
-}, false);
+function fixReferrer(footer){
+	footer.contentWindow.postMessage( ['referrer', window.location.href], '*' );
+}
