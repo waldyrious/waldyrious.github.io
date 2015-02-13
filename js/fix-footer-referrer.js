323595e8365aed80d42979bf93880363be4edab2
Waldir Pimenta <waldyrious@gmail.com>
2015-02-13 18:20:52 +0000

    urgh....

--- js/fix-footer-referrer.js
+++ js/fix-footer-referrer.js
@@ -2,8 +2,7 @@
 // Should be called "referer", but I refuse to obey a spec term
 // that is based on a misspelling: https://en.wiktionary.org/wiki/referer#Usage_notes
 
-var footer = document.getElementById('footer');
-footer.onload = function() {
+document.getElementById('footer').onload = function() {
   console.log(window.location.href);
-  footer.contentWindow.postMessage( ['referrer', window.location.href], '*' );
+  this.contentWindow.postMessage( ['referrer', window.location.href], '*' );
 }
